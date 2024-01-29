bd47ab97fdd37ee5ba7a1c6b4ad3b6daaf2afdd8
Savi8093 <149703164+Savi8093@users.noreply.github.com>
2024-01-29 10:12:10 +0100

    update

--- homework9/person_table.test.ts
+++ homework9/person_table.test.ts
@@ -1,3 +1,28 @@
-import { List, Pair } from '../lib/list';
-import { ProbingHashtable, ph_empty, ph_delete, ph_insert, ph_lookup, probe_linear } from '../lib/hashtables';
+import { toHashtable, People, Relations } from '../homework9/person_table';
+import { pair, list } from '../lib/list'
 
+test('my test', () => {
+    expect(validateTest1()).toBe(true);
+});
+
+function validateTest1() : boolean {
+
+    var test: People = list(
+        pair(19821208, "Oliver"), 
+        pair(19941208, "Erik"),
+        pair(19741208, "Anders"),
+        pair(20041208, "Johanna"),
+        pair(19451208, "Sandra")
+    );
+
+    var relations: Relations = list(
+        pair(19821208, 19941208),
+        pair(19821208, 19741208),
+        pair(19821208, 19451208)
+    );
+
+    let response = toHashtable(test, relations);
+    console.log(JSON.stringify(response));
+
+    return true;
+}
